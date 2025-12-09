4e99e1805de6d9d17d61957f9132747644dbb4d2
Ana Molina <ana713w@gmail.com>
2025-12-09 19:34:50 +0100

    fix/connection db deleted

--- routes/product.py
+++ routes/product.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, HTTPException, status
 from models.schemas import ProductCreate, ProductItemOrder
+from configuration.connections import db
+from mysql.connector import Error
 
 router = APIRouter(
     prefix="/product",
     tags=["Product"]
 )
+
 
